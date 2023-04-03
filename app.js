a98a170b6bc2aa8c1d3c10d8e8825f312d7e98f4
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-04-03 14:25:34 +0000

    updating

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ let myName=prompt("Please Enter Your Name");
 let gender=prompt("Enter Your gender");
 gender=gender.toUpperCase();
 
-for (var i = 0; i < Infinity; i++) {
+
     if (gender !== 'MALE' && gender !=='FEMALE'){
         gender=prompt("Please Enter a correct gender MALE or FEMALE");
         gender=gender.toUpperCase();
     
     }
- else {break;}
-}
+
+
 let age=prompt("Please Enter Your age");
 if(age<= 0){
     age=prompt("Your age is less than or equall Zero!! Enter your correct age");
 }
 let showWelcomingMsg=confirm('Do you want to show a welcoming message?');
 if(showWelcomingMsg){
-let A;
+
 if(gender == 'MALE'){
-    A='Mr'
+    alert(`Hello Mr ${myName} your age is ${age} confirm to skip this messge`);
 } else if(gender=='FEMALE')
-{A='Ms'}
+{alert(`Hello Ms ${myName} your age is ${age} confirm to skip this messge`);}
+else {alert(`Hello ${myName} your Gender is undefind and your age is ${age} confirm to skip this messge`);}
 
 
-alert(`Hello ${A} ${myName} your age is ${age} confirm to skip this messge`);}
+}
