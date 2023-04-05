719a2afc54742d3ac0c4876a095d93112cd09f88
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-04-05 08:02:45 +0000

    upadting

--- app.js
+++ app.js
@@ -25,5 +25,24 @@ if(gender == 'MALE'){
 else {alert(`Hello ${myName} your Gender is undefind and your age is ${age} confirm to skip this messge`);}
 }
 
-                           ////// LAB 06 ///////
-                           
+                           ////// LAB 06 ////
+
+ let like=prompt("do you like our websit?? (answer Yes/No)");
+ let simple= prompt("do our websit very simple??? (answer Yes/No)");
+ let develop= prompt("should we have to develop our websit as soon as possible?? (answer Yes/No)");
+ 
+ alert(` hiii ${like} ${simple} ${develop}`);
+ like= checkAnswer(like);
+simple= checkAnswer(simple);
+develop=checkAnswer(develop);
+const answerArray=[like,simple,develop];
+
+
+function checkAnswer(answer){
+    if (answer.toUpperCase() !== 'YES' && answer.toUpperCase() !=='NO'){
+        
+      return "invalid";
+    }
+     else {return (answer.toUpperCase());}
+}
+console.log(answerArray);
