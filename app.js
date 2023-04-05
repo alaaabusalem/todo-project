94642f99fc7a0afb8356717e53af0b1367506ea7
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-04-05 07:19:46 +0000

    creat to do js2

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict'
+let myName=prompt("Please Enter Your Name");
+let gender=prompt("Enter Your gender");
+gender=gender.toUpperCase();
+
+
+    if (gender !== 'MALE' && gender !=='FEMALE'){
+        gender=prompt("Please Enter a correct gender MALE or FEMALE");
+        gender=gender.toUpperCase();
+    
+    }
+
+
+let age=prompt("Please Enter Your age");
+if(age<= 0){
+    age=prompt("Your age is less than or equall Zero!! Enter your correct age");
+}
+let showWelcomingMsg=confirm('Do you want to show a welcoming message?');
+if(showWelcomingMsg){
+
+if(gender == 'MALE'){
+    alert(`Hello Mr ${myName} your age is ${age} confirm to skip this messge`);
+} else if(gender=='FEMALE')
+{alert(`Hello Ms ${myName} your age is ${age} confirm to skip this messge`);}
+else {alert(`Hello ${myName} your Gender is undefind and your age is ${age} confirm to skip this messge`);}
+}
+
+                           ////// LAB 06 ///////
+                           
